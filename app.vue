ffc71c81eede1751bfcb55964ca731e30ede7b4d
muumin <harada.kaito.20@shizuoka.ac.jp>
2024-09-20 19:04:31 +0900

    [rm] キャッシュ削除

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup="ts">
+// ページタイトルの設定
+useHead({
+  titleTemplate: (title) => {
+    return title
+      ? `${title} | No MUSIC No Travel`
+      : 'No Music No Travel'
+  },
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
